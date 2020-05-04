d18ba82b2c333d182e134e0cbf2b0eb2a4f18125
eugenio <eperedo13@alumnos.utalca.cl>
2020-05-03 21:51:38 -0300

    se agregaron atributos a db

--- src/hola_mundo/DB.java
+++ src/hola_mundo/DB.java
@@ -10,5 +10,19 @@ package hola_mundo;
  * @author keno
  */
 public class DB {
+    private String conexion;
+
+    public DB(String conexion) {
+        this.conexion = conexion;
+    }
+
+    public String getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(String conexion) {
+        this.conexion = conexion;
+    }
+    
     
 }
